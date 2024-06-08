cb1bdcca79db8904e078b4a5d4160d6766722200
cerdowzard <holuwayinkzzy@gmail.com>
2024-06-08 15:41:59 +0100

    new commit

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,32 @@
-use axum::{response::{self, IntoResponse},routing::{self, get},Json,Router};
+mod handler;
+mod model;
+mod route;
+mod schema;
+
 use std::sync::Arc;
+
+use axum::http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
+};
 use dotenv::dotenv;
+use route::create_router;
+use tower_http::cors::CorsLayer;
 
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
-async fn heath_checker_handler() -> impl IntoResponse {
-    const MESSAGE:&str= "heath_checker handler";
-
-    let json_response = serde_json::json!(
-        {
-            "code": 200,
-            "status": "OK",
-            "message": MESSAGE
-        }
-    );
-    Json(json_response)
-}
 
 pub struct AppState {
-    db:Pool<Postgres>
+    db: Pool<Postgres>,
 }
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set");
-    let pool = match PgPoolOptions::new() 
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = match PgPoolOptions::new()
         .max_connections(10)
-        .connect(&db_url)
+        .connect(&database_url)
         .await
     {
         Ok(pool) => {
@@ -35,12 +36,18 @@ async fn main() {
         Err(err) => {
             println!("🔥 Failed to connect to the database: {:?}", err);
             std::process::exit(1);
-        } 
+        }
     };
-    let app_state = Arc::new(AppState { db: pool.clone() });
-    let app = Router::new().route("/", get(heath_checker_handler));
+
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+
+    let app = create_router(Arc::new(AppState { db: pool.clone() })).layer(cors);
+
     println!("🚀 Server started successfully");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-
 }
